48b4bbe11bf0ab9788094f65dd07255fa50be62a
yasin taghavi <mrtqvi@yahoo.com>
2025-04-24 22:47:10 +0330

    add models

---
+++ app/Models/Group.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Group extends Model
+{
+    use HasUlids;
+
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
