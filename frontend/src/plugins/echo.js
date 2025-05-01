f4f44c99dfc0bceba725edd3292cecb261ce14b0
yasin taghavi <mrtqvi@yahoo.com>
2025-05-01 12:04:01 +0330

    fix: read pusher from env

--- frontend/src/plugins/echo.js
+++ frontend/src/plugins/echo.js
@@ -5,11 +5,11 @@ window.Pusher = Pusher;
 
 const echo = new Echo({
     broadcaster: 'pusher',
-    key: '95c4470e82ccf17e4725',
-    cluster: 'us3',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: false,
     encrypted: true,
-    authEndpoint: "http://localhost:8000/broadcasting/auth",
+    authEndpoint: import.meta.env.VITE_API_BASE_URL + "/broadcasting/auth",
     auth: {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('auth_token')}`,
